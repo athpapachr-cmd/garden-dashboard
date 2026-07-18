05bf56d3005fd29aeb3c832b40186a36f2b7320f
athpapachr-cmd <athpapachr@gmail.com>
2026-07-18 07:47:09 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const C='garden-care-v2';const A=['./','./index.html','./manifest.webmanifest','./assets/bed_tomatoes.jpeg','./assets/bed_peppers.jpeg'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
